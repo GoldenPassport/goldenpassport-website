195557545a1ba644b6300f52e9e5b607f376628b
ch-lukas <lukepa@hotmail.co.uk>
2026-01-01 23:55:03 +0000

    Fix i18n: move locales back to i18n/locales, use ./i18n/locales path

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,7 +27,7 @@ export default defineNuxtConfig({
     ],
     defaultLocale: 'en',
     lazy: true,
-    langDir: 'locales',
+    langDir: './i18n/locales',
     strategy: 'no_prefix',
     vueI18n: './i18n/i18n.config.ts',
     detectBrowserLanguage: {
